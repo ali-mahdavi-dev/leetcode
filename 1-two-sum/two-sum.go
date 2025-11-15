a3d3200077718cef1c6d2df5838cc35acab19d05
ali mahdavi <dev.alimahdavi@gmail.com>
2025-11-15 15:40:45 +0330

    Time: 4 ms (38.14%) | Memory: 5.9 MB (62.72%) - LeetSync

---
+++ 1-two-sum/two-sum.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+    sumMap:=map[int]int{}
+    
+    for i,v:=range nums{
+        if ei, ok:=sumMap[target-v]; ok{
+            return []int{ei, i}
+        }
+        sumMap[v]=i
+    }
+
+    return []int{}
+}
